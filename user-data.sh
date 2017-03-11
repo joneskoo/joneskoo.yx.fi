df5aeb3f05f5f62eb564a16986e647b2fca101b3
Joonas Kuorilehto <joneskoo@derbian.fi>
2017-03-11 16:52:06 +0200

    Allow updating installation script keeping user-data

--- user-data.sh
+++ user-data.sh
@@ -1,26 +1,3 @@
 #!/bin/bash
-set -eux
-mkdir -p /etc/caddy /etc/ssl/caddy
-chown -R root:www-data /etc/caddy
-chown -R www-data:root /etc/ssl/caddy
-chmod 0770 /etc/ssl/caddy
-
-curl -fsSL https://getcaddy.com | bash -s git,hugo
-curl -fsSL -o /etc/systemd/system/caddy.service https://raw.githubusercontent.com/mholt/caddy/master/dist/init/linux-systemd/caddy.service
-curl -fsSL -o /etc/caddy/Caddyfile https://raw.githubusercontent.com/joneskoo/joneskoo.yx.fi/master/Caddyfile
-
-chown root:root /etc/systemd/system/caddy.service
-chmod 744 /etc/systemd/system/caddy.service
-
-mkdir -p /var/www /var/www/joneskoo.yx.fi
-chown -R www-data:www-data /var/www
-chmod -R a+rX /var/www
-
-apt-get update && apt-get install -qyy python-pygments
-wget https://github.com/spf13/hugo/releases/download/v0.19/hugo_0.19-64bit.deb
-dpkg -i hugo_0.19-64bit.deb
-
-systemctl enable caddy.service
-systemctl daemon-reload
-systemctl start caddy.service
-
+set -e
+curl -fsSL https://raw.githubusercontent.com/joneskoo/joneskoo.yx.fi/master/bootstrap.sh | bash
