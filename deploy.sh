c27b58220523ef384bfd789fddf0cbc393908df8
Joonas Kuorilehto <joneskoo@derbian.fi>
2020-09-01 01:24:14 +0300

    Update deploy script for rsync

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 DIR=$(dirname "$0")
 
 cd $DIR
@@ -16,17 +18,14 @@ mkdir public
 git worktree prune
 rm -rf .git/worktrees/public/
 
-echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public origin/gh-pages
+echo "Checking out public branch into public"
+#git worktree add -B public public origin/public
+git worktree add -B public public public
 
 echo "Removing existing files"
 rm -rf public/*
 
 echo "Generating site"
 hugo
-echo joneskoo.yx.fi > public/CNAME
-touch public/.nojekyll
 
-echo "Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
-git push origin gh-pages
+rsync -Pvr public/ caddy-01.hez.yx.fi:/www/joneskoo.yx.fi/
